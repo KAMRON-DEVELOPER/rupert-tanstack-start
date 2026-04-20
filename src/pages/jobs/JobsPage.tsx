fec317d0ca67d076f5d3e08977cdc284fa33ee0e
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2026-04-20 14:45:04 +0500

    initial commit

--- src/pages/jobs/JobsPage.tsx
+++ src/pages/jobs/JobsPage.tsx
@@ -3,11 +3,13 @@ import JobsLeftSidebar from './JobsLeftSidebar';
 import JobsRightSidebar from './JobsRightSidebar';
 
 const JobsPage = () => {
-  <div className='grid md:grid-cols-5 min-h-screen'>
-    <JobsLeftSidebar />
-    <JobsContent />
-    <JobsRightSidebar />
-  </div>;
+  return (
+    <div className='grid md:grid-cols-5 min-h-screen'>
+      <JobsLeftSidebar />
+      <JobsContent />
+      <JobsRightSidebar />
+    </div>
+  );
 };
 
 export default JobsPage;
