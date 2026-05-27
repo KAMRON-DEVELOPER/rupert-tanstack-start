1b8de256799dfbcd8b98848a0a2a0b4c2de892b7
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2026-05-27 10:26:54 +0500

    small changes

--- src/routes/(public)/index.tsx
+++ src/routes/(public)/index.tsx
@@ -1,16 +1,16 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import HomePage from '@/pages/home/HomePage'
 import { useGetStatsQueryOptions } from '@/services/stats/stats'
 
 export const Route = createFileRoute('/(public)/')({
-  beforeLoad: ({ context: { isAuthenticated } }) => {
-    if (isAuthenticated) {
-      throw redirect({
-        to: '/work/vacancies',
-        replace: true
-      })
-    }
-  },
+  // beforeLoad: ({ context: { isAuthenticated } }) => {
+  //   if (isAuthenticated) {
+  //     throw redirect({
+  //       to: '/work/vacancies',
+  //       replace: true
+  //     })
+  //   }
+  // },
   loader: async ({ context: { queryClient } }) => {
     await Promise.all([queryClient.ensureQueryData(useGetStatsQueryOptions())])
   },
